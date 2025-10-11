4f521ae76ca317a3071b5e6d5fa47b98f4629323
Akorede Azeez <akoredeazeeztemiloluwa@gmail.com>
2025-10-11 20:51:23 +0100

    edited the navbar and hero section to make the logo better

--- frontend/my-ngo-frontend/next.config.mjs
+++ frontend/my-ngo-frontend/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com', 'localhost'],
+    domains: ['res.cloudinary.com', 'localhost', 'integral-amusement-0416bbb889.media.strapiapp.com'],
   },
 };
 
